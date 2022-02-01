b43dd338129c4df80009ae7b553c62eb29ea5885
Mohammad <mhamed.mussa@gmail.com>
2022-02-01 16:51:35 -0600

    removing learnyounode files

--- my-first-io.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let buffer = fs.readFileSync(process.argv[2])
-let bufferFile = buffer.toString()
-const lineBreaks = bufferFile.split('\n');
-console.log(lineBreaks.length - 1)
